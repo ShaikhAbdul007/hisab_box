960025b0e407726815cccd9bd3ee1603c3618976
abdul <rehmanshaikh0796@gmail.com>
2025-11-16 00:41:45 +0530

    key added

---
+++ lib/keys/keys.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+final shopDetails = GlobalKey<FormState>();
+final shopAddress = GlobalKey<FormState>();
+final userProfile = GlobalKey<FormState>();
+final loginkey = GlobalKey<FormState>();
+final categoryKey = GlobalKey<FormState>();
+final disountKey = GlobalKey<FormState>();
+final expenseKey = GlobalKey<FormState>();
+final inventoryScanKey = GlobalKey<FormState>();
+final formkeys = GlobalKey<FormState>();
