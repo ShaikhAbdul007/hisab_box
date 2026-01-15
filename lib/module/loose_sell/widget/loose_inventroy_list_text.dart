55c00c298bbf54b8e33b2ff3ed2b2e1c990d2a50
abdul <rehmanshaikh0796@gmail.com>
2026-01-16 00:07:39 +0530

    condition based Ui changes done

--- lib/module/loose_sell/widget/loose_inventroy_list_text.dart
+++ lib/module/loose_sell/widget/loose_inventroy_list_text.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:inventory/common_widget/colors.dart';
 import 'package:inventory/common_widget/common_padding.dart';
 import 'package:inventory/common_widget/size.dart';
-import 'package:inventory/helper/set_format_date.dart';
 import 'package:inventory/helper/textstyle.dart';
 import 'package:inventory/module/loose_sell/model/loose_model.dart';
 
@@ -21,120 +21,226 @@ class LooseInventroyListText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int color = int.parse(inventoryModel.color ?? '0');
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColors.whiteColor,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      margin: SymmetricPadding(horizontal: 15, vertical: 5).getPadding(),
-      child: Row(
-        children: [
-          Container(
-            height: 100,
-            width: 70,
-            decoration: BoxDecoration(
-              color: Color(color),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(5),
-                bottomLeft: Radius.circular(5),
-              ),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Quantity',
-                  style: CustomTextStyle.customPoppin(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.whiteColor,
-                  ),
-                ),
-                Text(
-                  inventoryModel.quantity.toString(),
-                  style: CustomTextStyle.customPoppin(
-                    color: AppColors.whiteColor,
-                  ),
-                ),
-              ],
-            ),
+    String rack = inventoryModel.rack ?? '';
+    String level = inventoryModel.level ?? '';
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppColors.whiteColor,
+            borderRadius: BorderRadius.circular(5),
           ),
-          setWidth(width: 5),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  inventoryModel.name ?? '',
-                  style: CustomTextStyle.customPoppin(),
+          margin: SymmetricPadding(horizontal: 15, vertical: 5).getPadding(),
+          child: Row(
+            children: [
+              Container(
+                margin: SymmetricPadding(horizontal: 5).getPadding(),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(5),
+                    bottomLeft: Radius.circular(5),
+                  ),
                 ),
-                setHeight(height: 5),
-                Row(
+                child: Icon(CupertinoIcons.cube, size: 27),
+              ),
+              setWidth(width: 5),
+              Expanded(
+                flex: 3,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        inventoryModel.name ?? '',
+                        style: CustomTextStyle.customPoppin(fontSize: 17),
+                      ),
+                      if (inventoryModel.flavor case ('' || null)) ...{
+                        Container(),
+                      } else ...{
+                        setHeight(height: 2),
+                        Text(
+                          inventoryModel.flavor ?? '',
+                          style: CustomTextStyle.customOpenSans(
+                            color: AppColors.greyColor,
+                          ),
+                        ),
+                      },
+                      Row(
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              text: '${inventoryModel.animalType} ',
+                              style: CustomTextStyle.customOpenSans(
+                                color: AppColors.greyColor,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: '${inventoryModel.weight} ',
+                                  style: CustomTextStyle.customOpenSans(
+                                    color: AppColors.greyColor,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: '${inventoryModel.category}  ',
+                                  style: CustomTextStyle.customOpenSans(
+                                    color: AppColors.greyColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            level.isNotEmpty && rack.isNotEmpty
+                                ? '${inventoryModel.location}/$level/$rack'
+                                : level.isEmpty && rack.isNotEmpty
+                                ? '${inventoryModel.location}/$rack'
+                                : rack.isEmpty && level.isNotEmpty
+                                ? '${inventoryModel.location}/$level'
+                                : '${inventoryModel.location}',
+                            style: CustomTextStyle.customOpenSans(
+                              color: AppColors.greyColor,
+                            ),
+                          ),
+                          setWidth(width: 10),
+                          inventoryModel.isLoosed ?? false
+                              ? Text(
+                                'Loosed : ${inventoryModel.isLoosed}',
+                                style: CustomTextStyle.customOpenSans(
+                                  color: AppColors.redColor,
+                                ),
+                              )
+                              : Container(),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            inventoryModel.purchaseDate ?? '',
+                            style: CustomTextStyle.customOpenSans(
+                              color: AppColors.greyColor,
+                            ),
+                          ),
+                          Text(
+                            ' - ',
+                            style: CustomTextStyle.customOpenSans(
+                              color: AppColors.greyColor,
+                            ),
+                          ),
+                          Text(
+                            inventoryModel.expireDate ?? '',
+                            style: CustomTextStyle.customOpenSans(
+                              color: AppColors.redColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      setHeight(height: 5),
+                      inventoryModel.quantity! > 0 &&
+                                  inventoryModel.quantity! < 10 ||
+                              inventoryModel.quantity! == 0
+                          ? Container(
+                            height: 20.h,
+                            width: 150.w,
+                            padding:
+                                SymmetricPadding(horizontal: 5).getPadding(),
+                            decoration: BoxDecoration(
+                              color: getColor(),
+                              borderRadius: BorderRadius.circular(5.r),
+                            ),
+                            child: Row(
+                              spacing: 5,
+                              children: [
+                                Icon(
+                                  Icons.info,
+                                  size: 15.sp,
+                                  color: AppColors.whiteColor,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    getText(),
+                                    style: CustomTextStyle.customOpenSans(
+                                      color: AppColors.whiteColor,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                          : SizedBox(),
+                      inventoryModel.quantity! > 0 &&
+                                  inventoryModel.quantity! < 10 ||
+                              inventoryModel.quantity! == 0
+                          ? setHeight(height: 5)
+                          : SizedBox(),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
                   children: [
-                    Text(
-                      inventoryModel.category ?? '',
-                      style: CustomTextStyle.customPoppin(
-                        color: AppColors.greyColor,
-                      ),
-                    ),
-                    setWidth(width: 10),
-                    Text(
-                      inventoryModel.weight ?? '',
-                      style: CustomTextStyle.customPoppin(
-                        color: AppColors.greyColor,
-                      ),
-                    ),
-                    setWidth(width: 10),
-                    Text(
-                      inventoryModel.animalType ?? '',
-                      style: CustomTextStyle.customPoppin(
-                        color: AppColors.greyColor,
-                      ),
-                    ),
-                    setWidth(width: 10),
                     Text(
                       '\u{20B9} ${inventoryModel.sellingPrice}',
                       style: CustomTextStyle.customPoppin(
                         color: AppColors.blackColor,
-                        fontSize: 16,
+                        fontSize: 18,
                       ),
                     ),
-                  ],
-                ),
-                setHeight(height: 5),
-                Text(
-                  inventoryModel.flavor ?? '',
-                  style: CustomTextStyle.customPoppin(
-                    color: AppColors.greyColor,
-                  ),
-                ),
-                setHeight(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      getStringLengthText(inventoryModel.barcode ?? ''),
-                      style: CustomTextStyle.customPoppin(
-                        color: AppColors.greyColor,
-                      ),
-                    ),
-                    isInventoryScanSelected
-                        ? InkWell(
-                          onTap: onTap,
-                          child: CustomPadding(
-                            paddingOption: OnlyPadding(right: 20.0),
-                            child: Icon(CupertinoIcons.square_pencil),
+                    RichText(
+                      text: TextSpan(
+                        text: inventoryModel.quantity.toString(),
+                        style: CustomTextStyle.customOpenSans(
+                          color: AppColors.blackColor,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: ' in stock',
+                            style: CustomTextStyle.customOpenSans(
+                              color: AppColors.greyColor,
+                            ),
                           ),
-                        )
-                        : Container(),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
+  }
+
+  String getText() {
+    String? text;
+    if (inventoryModel.quantity! > 0 && inventoryModel.quantity! < 10) {
+      text = 'Low Stock';
+    } else if (inventoryModel.quantity! == 0) {
+      text = 'Out of Stock';
+    }
+    return text ?? '';
+  }
+
+  Color getColor() {
+    Color? colors;
+    if (inventoryModel.quantity! > 0 && inventoryModel.quantity! < 10) {
+      colors = AppColors.greyColor;
+    } else if (inventoryModel.quantity! == 0) {
+      colors = AppColors.redColor;
+    } else {
+      colors = AppColors.blackColor;
+    }
+    return colors;
   }
 }
